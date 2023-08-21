afa5e87055d90e5dc6e01ea025e29baa4bedfbf7
jelal1cam <jh2402@cam.ac.uk>
2023-08-21 15:47:14 +0100

    unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] +quote['top_ask']['price']) * 0.5))
     
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
@@ -16,11 +18,38 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] +quote['top_ask']['price']) * 0.5))
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio(self) :
+    quotes_ratio = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    
+    quotes_zero = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+  
+    prices_ratio = {}
+    for quote in quotes_ratio:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      prices_ratio[stock] = price
+      print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+      print(prices_ratio)
 
+    self.assertEqual(getRatio(prices_ratio['ABC'], prices_ratio['DEF']), (prices_ratio['ABC']/prices_ratio['DEF']))
 
+    prices_zero = {}
+    for quote in quotes_zero:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      prices_zero[stock] = price
+      print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+      print(prices_zero)
+    self.assertRaises(ZeroDivisionError('Division by zero is not possible'), getRatio(prices_zero['ABC'], prices_zero['DEF']))
 
 if __name__ == '__main__':
     unittest.main()
